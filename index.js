c032eec9bada08237c80af6cb5c7b55bf6f68d5f
Teekay <tkmaile96@gmail.com>
2024-09-18 16:02:27 +0200

    debugged errors

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage'); //Made corrections
+      const courseNameInput = document.getElementById('courseName'); // made corrections
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -39,8 +39,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none';
     });
   });
   
